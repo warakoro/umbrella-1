abd00e193ee70c7f175aa2a584c05cf63e98b097
unknown <984272@CS_LAB113-108.cs.mum.edu>
2015-01-28 15:28:06 -0600

    Empty project again with temp Entity class

---
+++ Goalizer/Goalizer-ejb/src/java/com/umbrella/goalizer/entity/Test.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.umbrella.goalizer.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author 984272
+ */
+@Entity
+public class Test implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Test)) {
+            return false;
+        }
+        Test other = (Test) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.umbrella.goalizer.entity.Test[ id=" + id + " ]";
+    }
+    
+}
