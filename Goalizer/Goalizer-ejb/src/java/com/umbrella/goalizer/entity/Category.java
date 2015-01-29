79fb02a6a7af149a6b2c01429a1c2ff6abb51603
984372 <984372@CS_LAB113-111.cs.mum.edu>
2015-01-29 11:12:25 -0600

    Added entities customized

--- Goalizer/Goalizer-ejb/src/java/com/umbrella/goalizer/entity/Category.java
+++ Goalizer/Goalizer-ejb/src/java/com/umbrella/goalizer/entity/Category.java
@@ -84,29 +84,4 @@ public class Category implements Serializable {
         this.goalList = goalList;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Category)) {
-            return false;
-        }
-        Category other = (Category) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "mum.umbrella.goaltraker.Category[ id=" + id + " ]";
-    }
-    
 }
