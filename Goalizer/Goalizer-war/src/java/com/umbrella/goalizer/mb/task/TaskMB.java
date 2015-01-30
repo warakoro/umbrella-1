d04f6cd6cf6670147d691846f9b5d7837addb085
unknown <984272@CS_LAB113-108.cs.mum.edu>
2015-01-30 14:08:23 -0600

    Added TaskMB

---
+++ Goalizer/Goalizer-war/src/java/com/umbrella/goalizer/mb/task/TaskMB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.umbrella.goalizer.mb.task;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author 984272
+ */
+@ManagedBean
+@RequestScoped
+public class TaskMB {
+
+    /**
+     * Creates a new instance of TaskMB
+     */
+    public TaskMB() {
+    }
+    
+}
