1fbdc8673c55e9661f2dadbcecc5d781b9587d73
Mou Janki <unaruto1990@gmail.com>
2015-02-01 20:54:22 -0600

    Donya: added Goals and Goal pages with template

---
+++ Goalizer/Goalizer-war/src/java/com/umbrella/goalizer/mb/goal/GoalMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.umbrella.goalizer.mb.goal;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author mjanki
+ */
+@ManagedBean
+@RequestScoped
+public class GoalMB {
+
+    /**
+     * Creates a new instance of Goal
+     */
+    public GoalMB() {
+    }
+    
+}
